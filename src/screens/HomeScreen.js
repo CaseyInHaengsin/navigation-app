72c05ec4409f4ac0b15aee1de1dcd767674dfe42
CaseyInHaengsin <casey.rowley@kuali.co>
2021-01-31 17:33:22 -0700

    Starter code added for navigation app.

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text } from 'react-native-elements';
+import { StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <>
+            <Text h1>Hello World</Text>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default HomeScreen;
